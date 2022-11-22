fd77bf48e1f7bf006f17d99972a3f61259a15f5f
Chad Jemmett <cjemmett@wehco.com>
2022-11-22 15:04:22 -0600

    Update model. and stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap5',
+    'django_seed',
 ]
 
 MIDDLEWARE = [
--- shiren/views.py
+++ shiren/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.template import loader
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from .models import Item, Price
+from .models import Item
 from django.urls import reverse
 from django.views import generic
 from .forms import ItemForm
@@ -16,13 +16,14 @@ def index(request):
 
 
 def list(request):
-    value  = request.POST.get('price')
-    try:
-        price = get_object_or_404(Price, item_price=value)
-    except:
-        return render(request, "shiren/list_items.html", {})
+    pass
+    # value  = request.POST.get('price')
+    # try:
+    #     price = get_object_or_404(Item, item_price=value)
+    # except:
+    #     return render(request, "shiren/list_items.html", {})
 
-    items = Item.objects.filter(price_id=price.id)
-    context ={'items': items}
-    return render(request, "shiren/list_items.html", context)
+    # items = Item.objects.filter(price_id=price.id)
+    # context ={'items': items}
+    # return render(request, "shiren/list_items.html", context)
 
