b87cbe4ae7e0309d9731eb1b88aa72fdcfad99cf
Chad Jemmett <cjemmett@wehco.com>
2022-11-23 21:45:14 -0600

    Trying to use sqlite memory.

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,6 +91,7 @@ if DEVELOPMENT_MODE is True:
         "default": {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': ':memory:',
                     }
 
                 }
